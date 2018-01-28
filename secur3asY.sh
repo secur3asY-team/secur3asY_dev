1ea47cf8bf3c19099aca43ba40d8831d27038b65
Aastrom <antoinehenry.pro@outlook.fr>
2018-01-28 22:20:10 +0100

    Update

--- secur3asY.sh
+++ secur3asY.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-# Vérification des pré-requis
-sh /opt/secur3asY/scripts/check_secur3asY_ressources.sh
-sh /opt/secur3asY/scripts/check_evilgrade_ressources.sh
+if [ "$UID" -ne "0" ]
+then 	echo "\033[31;1m[!] \033[0;1mVeuillez lancer secur3asY en tant que root."
+	exit 1
+fi
 
-# Initialisation de Evilgrade
-sleep 1
-cd /opt/secur3asY/modules/isr-evilgrade
-./evilgrade
+# Vérification des pré-requis
+
+if [ -f "scripts/check_secur3asY_ressources.sh" ]
+then	sh scripts/check_secur3asY_ressources.sh
+else	echo "\033[31;1m[!] \033[0;1mRessource manquante : \033[31;1mcheck_secur3asY_ressources.sh\033[0;1m."
+	echo "\033[33;1m[>] \033[0;1mVeuillez recloner le dépôt de secur3asY."
+	exit 1
+fi
+
+if [ -f "scripts/check_dependancies.sh" ]
+then	sh scripts/check_dependancies.sh
+else    echo "\033[31;1m[!] \033[0;1mRessource manquante : \033[31;1mcheck_dependancies.sh\033[0;1m."
+        echo "\033[33;1m[>] \033[0;1mVeuillez recloner le dépôt de secur3asY."
+        exit 1
+fi
+
