a950b7bd28403750c4560c40d5fffd9d4ef20f68
root <root@Aastrom-DELL.lan>
2018-05-01 03:35:24 +0200

    check_interfaces ok

--- scripts/check_interfaces.sh
+++ scripts/check_interfaces.sh
@@ -10,12 +10,16 @@ text_red="\033[31;1m"
 text_green="\033[32;1m"
 
 # Remove temporary file 
-rm conf/network.conf && touch conf/network.conf
+if [ -e conf/network.conf ]
+then	rm conf/network.conf
+fi
+touch conf/network.conf
 
 declare -a ips
 declare -a macs
+declare -a interfaces_types
 
-printf "Checking interfaces..."
+printf "Checking interfaces... "
 
 interfaces_tab=$(ifconfig|grep -v "lo:"|grep ": "|awk -F ":" '{print $1}')
 if [ $? -eq 0 ]
@@ -31,20 +35,26 @@ for interface in $interfaces_tab
 do
 	ips[$i]=$(ifconfig $interface|grep "inet "|awk '{print $2}')
 	macs[$i]=$(ifconfig $interface|grep "ether"|awk '{print $2}')
+	iwconfig $interface > /dev/null 2>&1
+	if [ $? -eq 0 ]
+	then	interfaces_types[$i]="Wi-Fi"
+	else	interfaces_types[$i]="Ethernet"
+	fi
 	{
 		printf "# Interface $interface\\n"; 
 		printf "interface=%s\\n" $interface;
+		printf "interface_type=%s\\n" ${interfaces_types[$i]};
 		printf "ip_address=%s\\n" ${ips[$i]};
 		printf "mac_address=%s\\n" ${macs[$i]};
 		echo;
 	} >> conf/network.conf 
 	if [ "${ips[$i]}" != "" ]
 	then	
-		printf "[${text_green}ON${text_default}]  Interface : ${text_bold}$interface${text_default}\n"
+		printf "[${text_green}ON${text_default}]  Interface : ${text_bold}$interface${text_default} (${interfaces_types[$i]})\n"
 		printf "      IP address : ${text_green}${ips[$i]}${text_default}\n"
 		printf "      MAC address : ${macs[$i]}\n"
 	else
-		printf "[${text_red}OFF${text_default}]  Interface : ${text_bold}$interface${text_default}\n"
+		printf "[${text_red}OFF${text_default}]  Interface : ${text_bold}$interface${text_default} (${interfaces_types[$i]})\n"
 		printf "       MAC address : ${macs[$i]}\n"
 	fi
 	printf "\r\n"
