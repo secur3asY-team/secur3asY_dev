d19b511d180a7ea0cf2a1fd9e9df1d0546f1bf99
Aastrom <antoinehenry.pro@outlook.fr>
2018-05-10 20:16:23 +0200

    Installation and interfaces check ok

---
+++ scripts/check_installation.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+# @author : 		Antoine HENRY
+# @lastUpdate :		2018-03-22
+# @role :			Check secur3asY installation
+
+text_default="\033[0m"
+text_bold="\033[1m"
+text_red="\033[31;1m"
+text_green="\033[32;1m"
+text_yellow="\033[33;1m"
+text_blue="\033[34;1m"
+
+write_well () {
+		for i in $(seq 1 ${#1});
+		do		printf "$(printf "$1"|cut -c$i)"
+				sleep .006
+		done
+		printf "\\n"
+}
+
+write_well_without_return () {
+		for i in $(seq 1 ${#1});
+		do		printf "$(printf "$1"|cut -c$i)"
+				sleep .002
+		done
+}
+
+ACTUAL_PATH="$(pwd)"
+CONF_PATH="$ACTUAL_PATH/conf"
+SECUR3ASY_CONF_PATH="$CONF_PATH/secur3asY.conf"
+
+# secur3asY configuration file generation for first install
+
+if [ ! -d "$CONF_PATH" ]
+then
+		mkdir -p "$CONF_PATH"
+		chown -R root:root "$CONF_PATH" && chmod -R 770 "$CONF_PATH"
+fi
+
+if [ ! -f "$SECUR3ASY_CONF_PATH" ]
+then
+		SECUR3ASY_DEFAULT_PATH=/opt/secur3asY
+		CACHE_DEFAULT_PATH=$SECUR3ASY_DEFAULT_PATH/cache
+		LOG_DEFAULT_PATH=$CACHE_DEFAULT_PATH/log
+		REPORT_DEFAULT_PATH=$CACHE_DEFAULT_PATH/report
+		{ 
+				printf "# --- secur3asY default configuration file ---\\n";
+				echo;
+				printf "# Edit this line to change manually secur3asY installation folder.\\n";
+				printf "SECUR3ASY_PATH=%s\\n" $SECUR3ASY_DEFAULT_PATH;
+				echo;
+				printf "# Edit these lines to change manually cache folders location.\\n";
+				printf "CACHE_PATH=%s\\n" $CACHE_DEFAULT_PATH;
+				printf "LOG_PATH=%s\\n" $LOG_DEFAULT_PATH;
+				printf "REPORT_PATH=%s\\n" $REPORT_DEFAULT_PATH
+				echo;
+		}> "$SECUR3ASY_CONF_PATH"
+fi
+
+# Load secur3asY configuration file and check installation
+script_location="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+. "$SECUR3ASY_CONF_PATH"
+
+printf "Checking installation... "
+
+if [ ! -d "$SECUR3ASY_PATH" ]
+then
+		write_well "${text_red}NOK${text_default}\\n"
+		echo
+		write_well "[${text_red}x${text_default}] secur3asY is not installed on the system.\r\n"
+		echo
+		echo "----------------------------------------------------"
+		echo
+		write_well "       ${text_red}[ --- secur3asY installation --- ]${text_default}\\n"
+		echo
+		write_well "[${text_blue}i${text_default}] Default installation folder : $SECUR3ASY_PATH\n"
+		write_well_without_return "[${text_yellow}?${text_default}] Install secur3asY into default installation folder ? [Y/n] :"
+		read -p " " choice1
+		case $choice1 in
+				'O'|'o'|'y'|'Y')
+						echo		
+						write_well "[${text_green}+${text_default}] secur3asY installation folder : $SECUR3ASY_PATH\\n";;
+				'N'|'n')
+						write_well "[${text_yellow}?${text_default}] Please type secur3asY installation folder location :" 
+						read -p " " CHOSEN_PATH
+						if [ -d "$CHOSEN_PATH" ]
+						then
+								NEW_SECUR3ASY_PATH=$CHOSEN_PATH/secur3asY
+								write_well "[${text_yellow}-${text_default}] Please confirm secur3asY installation folder $NEW_SECUR3ASY_PATH : " 
+								read -p "[Y/n] " choice2
+								case $choice2 in
+								'O'|'o'|'y'|'Y')  
+										NEW_CACHE_PATH=$NEW_SECUR3ASY_PATH/cache
+										NEW_LOG_PATH=$NEW_CACHE_PATH/log
+										NEW_REPORT_PATH=$NEW_CACHE_PATH/report
+										echo
+										write_well "[${text_green}+${text_default}] secur3asY installation folder : $NEW_SECUR3ASY_PATH\r\n\r\n"
+										cp $SECUR3ASY_CONF_PATH $CONF_PATH/default.conf
+										{
+											printf "# --- secur3asY current configuration file ---\\r\\n";
+											printf "# Edit this line to change manually secur3asY installation folder.\\n";
+											printf "SECUR3ASY_PATH=%s" $NEW_SECUR3ASY_PATH;
+											echo;
+											printf "# Edit these lines to change manually cache folders location.\\n";
+											printf "CACHE_PATH=%s" $NEW_CACHE_PATH;
+											echo;
+											printf "LOG_PATH=%s" $NEW_LOG_PATH;
+											echo;
+											printf "REPORT_PATH=%s" $NEW_REPORT_PATH;
+										}> "$SECUR3ASY_CONF_PATH";;
+
+									'N'|'n')  
+											write_well "[${text_red}x${text_default}] Please retry later.\n"
+										  	exit 1;;
+								esac
+						else
+								write_well "[${text_red}x${text_default}] Expected directory does not exist.\n"
+								write_well "[${text_red}-${text_default}] Please retry later.\n"
+								exit 1
+						fi;;
+		esac
+
+		. "$SECUR3ASY_CONF_PATH"
+
+		echo
+		write_well_without_return "Creating secur3asY folders... "
+		mkdir -p "$SECUR3ASY_PATH" && mkdir -p "$CACHE_PATH" && mkdir -p "$LOG_PATH" && mkdir -p "$REPORT_PATH" && chown -R root:root "$SECUR3ASY_PATH" && chmod -R 770 "$SECUR3ASY_PATH"
+		if [ $? -eq 0 ]
+		then	write_well "${text_green}OK${text_default}\\n"
+		else	write_well "${text_red}NOK${text_default}\\n"
+				exit 1
+		fi
+
+		write_well_without_return "Copying secur3asY files... "
+		cp -R conf "$SECUR3ASY_PATH" && cp -R scripts "$SECUR3ASY_PATH" && cp -R custom "$SECUR3ASY_PATH" && cp secur3asY.sh "$SECUR3ASY_PATH"
+		if [ $? -eq 0 ]
+		then	write_well "${text_green}OK${text_default}\\n"
+		else	write_well "${text_red}NOK${text_default}\\n"
+				exit 1
+		fi
+
+		echo 
+		write_well "[${text_green}OK${text_default}] secur3asY installation succeeded ! :)\\n"
+		sleep 2
+		echo
+		echo "----------------------------------------------------"
+		echo
+		script_location="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+		if [ "$script_location" != "$SECUR3ASY_PATH" ]
+		then 	bash $SECUR3ASY_PATH/secur3asY.sh
+				exit 1
+		fi
+else
+		printf "${text_green}OK${text_default}\\n"
+		if [ "$script_location" != "$SECUR3ASY_PATH" ]
+		then 	bash $SECUR3ASY_PATH/secur3asY.sh
+				exit 1
+		fi
+		if [ ! -d "$CACHE_PATH" ]
+		then 	
+				mkdir -p "$CACHE_PATH"
+		fi
+
+		if [ ! -d "$LOG_PATH" ]
+		then    mkdir -p "$LOG_PATH"
+		fi
+
+		if [ ! -d "$REPORT_PATH" ]
+		then    mkdir -p "$REPORT_PATH"
+		fi
+fi
--- scripts/check_interfaces.sh
+++ scripts/check_interfaces.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # @author :			Melodie BERNARD, Antoine HENRY
 # @lastUpdate :     2018-05-01
@@ -8,29 +8,55 @@ text_default="\033[0m"
 text_bold="\033[1m"
 text_red="\033[31;1m"
 text_green="\033[32;1m"
+text_yellow="\033[33;1m"
+text_blue="\033[34;1m"
 
-# Remove temporary file 
+write_well () {
+		for i in $(seq 1 ${#1});
+		do		printf "$(printf "$1"|cut -c$i)"
+				sleep .006
+		done
+		printf "\\n"
+
+}
+
+write_well_without_return () {
+		for i in $(seq 1 ${#1});
+		do		printf "$(printf "$1"|cut -c$i)"
+				sleep .002
+		done
+}
+
+# Removes temporary file and creates the template
 if [ -e conf/network.conf ]
 then	rm conf/network.conf
 fi
-touch conf/network.conf
+{
+	printf "# --- secur3asy network configuration file ---\\n";
+	echo;
+} > conf/network.conf
 
 declare -a ips
 declare -a macs
 declare -a interfaces_types
 
-printf "Checking interfaces... "
+write_well_without_return "Checking interfaces... "
 
+# Stores all pertinent interfaces names from ifconfig into a string (tab of words) 
 interfaces_tab=$(ifconfig|grep -v "lo:"|grep ": "|awk -F ":" '{print $1}')
 if [ $? -eq 0 ]
-then	printf "${text_green}OK${text_default}\\n"
+then	write_well "${text_green}OK${text_default}\\n"
 		nbint=$(echo $interfaces_tab|wc -w)
-else	printf "${text_red}NOK${text_default}\\n"
+else	write_well "${text_red}NOK${text_default}\\n"
+		exit 1
 fi
 echo
 echo "----------------------------------------------------"
 echo
+i=0
 
+# Print then stores IP, MAC, and type of each interface founded just before 
+# into an INI-like configuration file, thanks to ifconfig and iwconfig.
 for interface in $interfaces_tab
 do
 	ips[$i]=$(ifconfig $interface|grep "inet "|awk '{print $2}')
@@ -41,12 +67,16 @@ do
 	else	interfaces_types[$i]="Ethernet"
 	fi
 	{
-		printf "# Interface $interface\\n"; 
-		printf "interface=%s\\n" $interface;
+		printf "[$interface]\\n"; 
+		printf "interface_name=%s\\n" $interface;
 		printf "interface_type=%s\\n" ${interfaces_types[$i]};
+		if [ "${ips[$i]}" != "" ]
+		then	printf "interface_status=on\\n";
+		else	printf "interface_status=off\\n";
+		fi
 		printf "ip_address=%s\\n" ${ips[$i]};
 		printf "mac_address=%s\\n" ${macs[$i]};
-		echo;
+		echo
 	} >> conf/network.conf 
 	if [ "${ips[$i]}" != "" ]
 	then	
@@ -58,4 +88,5 @@ do
 		printf "       MAC address : ${macs[$i]}\n"
 	fi
 	printf "\r\n"
+	i=$((i+1))
 done
